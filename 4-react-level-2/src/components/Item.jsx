ad5097aa36655f1ff9b6066b13ffba1a3505adfc
EzharAnsari <37619206+EzharAnsari@users.noreply.github.com>
2025-01-04 15:38:31 +0530

    learned css module

--- 4-react-level-2/src/components/Item.jsx
+++ 4-react-level-2/src/components/Item.jsx
@@ -1,7 +1,11 @@
+import style from "./Item.module.css";
+
 const Item = (props) => {
   return (
     <>
-      <li className="list-group-item">{props.foodItem}</li>
+      <li className={`list-group-item ${style["kg-item"]}`}>
+        <span className={`${style["kg-span"]}`}>{props.foodItem}</span>
+      </li>
     </>
   );
 };
