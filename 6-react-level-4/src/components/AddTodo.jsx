ba7e86d9ce7c479665cb2836f64b5fb2d2d41570
EzharAnsari <37619206+EzharAnsari@users.noreply.github.com>
2025-01-10 21:34:47 +0530

    Initial commit

---
+++ 6-react-level-4/src/components/AddTodo.jsx
@@ -0,0 +1,43 @@
+import { useRef } from "react";
+import { IoAddSharp } from "react-icons/io5";
+
+function AddTodo({ handleAddButtonClick }) {
+  const inputNameElement = useRef("");
+  const inputDateElement = useRef("");
+
+  const handleAddButton = (event) => {
+    console.log("clicked");
+    event.preventDefault();
+    const todoItem = {
+      name: inputNameElement.current.value,
+      dueDate: inputDateElement.current.value,
+    };
+    handleAddButtonClick(todoItem);
+    inputNameElement.current.value = "";
+    inputDateElement.current.value = "";
+  };
+
+  return (
+    <div className="container">
+      <form className="row kg-row" onSubmit={(e) => handleAddButton(e)}>
+        <div className="col-6">
+          <input
+            type="text"
+            placeholder="Enter Todo here"
+            ref={inputNameElement}
+          />
+        </div>
+        <div className="col-4">
+          <input type="date" ref={inputDateElement} />
+        </div>
+        <div className="col-2">
+          <button className="btn btn-success kg-button">
+            <IoAddSharp />
+          </button>
+        </div>
+      </form>
+    </div>
+  );
+}
+
+export default AddTodo;
---
+++ 6-react-level-4/src/components/TodoItems.jsx
@@ -0,0 +1,19 @@
+import TodoItem from "./TodoItem";
+
+const TodoItems = ({ items, handleDeleteBtn }) => {
+  // items.map((ele) => console.log(ele));
+  return (
+    <div className="items-container">
+      {items.map((item) => (
+        <TodoItem
+          key={item.name}
+          todoName={item.name}
+          todoDate={item.dueDate}
+          handleDeleteBtn={handleDeleteBtn}
+        ></TodoItem>
+      ))}
+    </div>
+  );
+};
+
+export default TodoItems;
