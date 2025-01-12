6e951f4a48710860dd9530342fb6894182ab3cbb
EzharAnsari <37619206+EzharAnsari@users.noreply.github.com>
2025-01-12 22:16:27 +0530

    data fetching using fetch

--- 7-react-level-5/src/components/PostList.jsx
+++ 7-react-level-5/src/components/PostList.jsx
@@ -1,12 +1,22 @@
 import Post from "./Post";
 import { PostList as PostListData } from "../store/post-list-store";
 import { useContext } from "react";
+import WelcomeMessage from "./WelcomeMessage";
 
 const PostList = () => {
-  const { postList } = useContext(PostListData);
+  const { postList, addInitialPost } = useContext(PostListData);
+
+  const handleGetPostsClick = () => {
+    fetch("https://dummyjson.com/posts")
+      .then((res) => res.json())
+      .then((data) => addInitialPost(data.posts));
+  };
 
   return (
     <>
+      {postList.length === 0 && (
+        <WelcomeMessage onGetPostsClick={handleGetPostsClick} />
+      )}
       {postList.map((post) => (
         <Post key={post.id} post={post} />
       ))}
--- 7-react-level-5/src/store/post-list-store.jsx
+++ 7-react-level-5/src/store/post-list-store.jsx
@@ -3,6 +3,7 @@ import { createContext, useReducer } from "react";
 export const PostList = createContext({
   postList: [],
   addPost: () => {},
+  addInitialPost: () => {},
   deletePost: () => {},
 });
 
@@ -14,15 +15,14 @@ const postListReducer = (currPostList, action) => {
     );
   } else if (action.type === "ADD_POST") {
     newCurrPostList = [action.payload.post, ...currPostList];
+  } else if (action.type === "ADD_INITIAL_POST") {
+    newCurrPostList = action.payload.posts;
   }
   return newCurrPostList;
 };
 
 const PostListContextProvider = ({ children }) => {
-  const [postList, dispatchPostList] = useReducer(
-    postListReducer,
-    DEFAULT_POST_LIST
-  );
+  const [postList, dispatchPostList] = useReducer(postListReducer, []);
 
   const addPost = (userId, postTitle, postBody, reactions, tags) => {
     dispatchPostList({
@@ -49,30 +49,22 @@ const PostListContextProvider = ({ children }) => {
     });
   };
 
+  const addInitialPost = (posts) => {
+    dispatchPostList({
+      type: "ADD_INITIAL_POST",
+      payload: {
+        posts,
+      },
+    });
+  };
+
   return (
-    <PostList.Provider value={{ postList, addPost, deletePost }}>
+    <PostList.Provider
+      value={{ postList, addPost, addInitialPost, deletePost }}
+    >
       {children}
     </PostList.Provider>
   );
 };
-
-const DEFAULT_POST_LIST = [
-  {
-    id: 1,
-    title: "Going to Mumbai",
-    body: "Hi Friends, I am going to Mumbai for my vacations. Hope to enjoy a lot. Peace out",
-    reactions: 2,
-    userId: "user-9",
-    tags: ["vacation", "Mumbai", "Enjoying"],
-  },
-  {
-    id: 2,
-    title: "pass ho gaye bhai",
-    body: "4 saal ki masti ke bad bhi ho gaye pass. Hard to believe",
-    reactions: 16,
-    userId: "user-7",
-    tags: ["Graduating", "Unbelievable"],
-  },
-];
 
 export default PostListContextProvider;
